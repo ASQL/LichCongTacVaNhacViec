1f501aa95baf942f6a2c39616defc9d8e074b00d
Công Phương Đông <gongfangdong@gmail.com>
2016-01-07 11:10:33 +0700

    Update

---
+++ DTO/Schedule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Schedule
+    {
+        private String id;
+
+        public String ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private String work;
+
+        public String Work
+        {
+            get { return work; }
+            set { work = value; }
+        }
+
+        private String detail;
+
+        public String Detail
+        {
+            get { return detail; }
+            set { detail = value; }
+        }
+
+        private String place;
+
+        public String Place
+        {
+            get { return place; }
+            set { place = value; }
+        }
+
+        private DateTime beginDate;
+
+        public DateTime BeginDate
+        {
+            get { return beginDate; }
+            set { beginDate = value; }
+        }
+
+        private DateTime endDate;
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+    }
+}
