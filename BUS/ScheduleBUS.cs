a5a2922b1c3cbf0c9a20f752faf4dba2094bd7dd
Công Phương Đông <gongfangdong@gmail.com>
2016-01-09 12:48:44 +0700

    Update

--- BUS/ScheduleBUS.cs
+++ BUS/ScheduleBUS.cs
@@ -27,5 +27,42 @@ namespace BUS
                 throw ex;
             }
         }
+
+        public DataTable GetAllBus()
+        {
+            return scheduleDAL.GetAll();
+        }
+
+        public bool InsertBus(String scheduleId, String work, String detail, String place, DateTime beginDate, DateTime endDate, String facultyId, String subjectId)
+        {
+            return scheduleDAL.Insert(scheduleId, work, detail, place, beginDate, endDate, facultyId, subjectId);
+        }
+
+        public bool UpdateBus(Schedule schedule)
+        {
+            return scheduleDAL.Update(schedule);
+        }
+        //public bool Delete()
+
+
+        public DataTable findScheduleByWorkBus(String work)
+        {
+            return scheduleDAL.findSchedulebyWork(work);
+        }
+
+        public string getLastIdBus()
+        {
+            return scheduleDAL.getLastId();
+        }
+
+        public string getFacultyId(string staffId)
+        {
+            return scheduleDAL.getFacultyId(staffId);
+        }
+
+        public string getSubjectId(string staffId)
+        {
+            return scheduleDAL.getSubjectId(staffId);
+        }
     }
 }
